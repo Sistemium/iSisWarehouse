6a3dd32ebb86f49b634a500258b88db3e6628fc5
Grigoriev Maxim <grigoriev.maxim@gmail.com>
2016-05-23 12:08:47 +0300

    add some controllers

---
+++ iSisWarehouse/Classes/EntityControllers/STMObjectsController.m
@@ -0,0 +1,17 @@
+//
+//  STMObjectsController.m
+//  iSistemium
+//
+//  Created by Maxim Grigoriev on 18/04/16.
+//  Copyright © 2016 Sistemium UAB. All rights reserved.
+//
+
+#import "STMObjectsController.h"
+
+#import "STMDataModel.h"
+
+
+@implementation STMObjectsController
+
+
+@end
---
+++ iSisWarehouse/Classes/ProcessesControllers/STMPickingOrdersProcessController.h
@@ -0,0 +1,53 @@
+//
+//  STMPickingOrdersProcessController.h
+//  iSistemium
+//
+//  Created by Maxim Grigoriev on 24/11/15.
+//  Copyright © 2015 Sistemium UAB. All rights reserved.
+//
+
+#import "STMController.h"
+
+@interface STMPickingOrdersProcessController : STMController
+
++ (void)updateVolumesWithIncreaseVolumeForPositionPicked:(STMPickingOrderPositionPicked *)positionPicked;
++ (void)updateVolumesWithDeletePositionPicked:(STMPickingOrderPositionPicked *)positionPicked;
+
++ (void)updateVolumesWithNewVolume:(NSInteger)newVolume
+                 forPositionPicked:(STMPickingOrderPositionPicked *)positionPicked;
+
++ (STMPickingOrderPositionPicked *)createPositionPickedForStockBatch:(STMStockBatch *)stockBatch
+                                                         andPosition:(STMPickingOrderPosition *)position
+                                                      withFullVolume:(BOOL)fullVolume
+                                                         barCodeScan:(STMBarCodeScan *)barCodeScan;
+
++ (void)linkBarCodeScan:(STMBarCodeScan *)barCodeScan
+     withPositionPicked:(STMPickingOrderPositionPicked *)positionPicked;
+
+
+
+#pragma mark - first version of picking process
+
+// ----
+// first version of picking process
+
++ (void)   position:(STMPickingOrderPosition *)position
+wasPickedWithVolume:(NSUInteger)volume
+  andProductionInfo:(NSString *)info
+         andBarCode:(NSString *)barcode;
+
++ (void)pickPosition:(STMPickingOrderPosition *)position
+      fromStockBatch:(STMStockBatch *)stockBatch
+         withBarCode:(NSString *)barcode;
+
++ (void)pickedPosition:(STMPickingOrderPositionPicked *)pickedPosition
+             newVolume:(NSUInteger)volume
+     andProductionInfo:(NSString *)info;
+
++ (void)deletePickedPosition:(STMPickingOrderPositionPicked *)pickedPosition;
+
+// end of first version of picking process
+// ----
+
+
+@end
